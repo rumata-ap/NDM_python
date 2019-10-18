791377fd0cd8e5c527c9f6a9d15d39fbb0bfd4a3
rumata-ap <avp.ponomarev@gmail.com>
2019-10-18 22:51:15 +0300

    edit

--- gui.py
+++ gui.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-import sys, copy, pickle
+import sys
+import copy
+import pickle
 from PyQt5 import QtCore, QtWidgets, QtGui
 #from modules.project import Project
 from modules.mainWindow import MyWindow
 #ptj = Project('tmp')
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
     #window.setWindowTitle("Класс QMainWindow")
