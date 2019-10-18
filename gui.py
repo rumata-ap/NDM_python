71e88f44dd0cd1b603786862ab11eb25da75df2f
rumata-ap <avp.ponomarev@gmail.com>
2019-10-18 22:49:11 +0300

    edit

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@
 import sys, copy, pickle
 from PyQt5 import QtCore, QtWidgets, QtGui
 #from modules.project import Project
-from modules.MainWindow import MyWindow
+from modules.mainWindow import MyWindow
 #ptj = Project('tmp')
 
 
